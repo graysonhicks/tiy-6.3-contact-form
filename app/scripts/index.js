27a588a3cf4eb071b1acb952722dac443ee9e06d
Grayson Hicks <graysonhicks@gmail.com>
2016-03-09 15:20:43 -0500

    added requires and model and collection

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,4 +2,15 @@ var $ = require('jquery');
 var handlebars = require('handlebars');
 var _ = require('underscore');
 
-console.log("Hello World!");
+var $ = require('jquery');
+var Backbone = require('backbone');
+var handlebars = require('handlebars');
+
+var ContactModel = Backbone.Model.extend({
+
+});
+
+var ContactCollection = Backbone.Collection.extend({
+  model: ContactModel,
+   }
+);
