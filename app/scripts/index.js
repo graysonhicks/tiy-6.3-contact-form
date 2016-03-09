119e6a959b339a38dcce76c2a738ca7d75d1a088
Dale Fenton <fentondale@gmail.com>
2016-03-09 15:41:10 -0500

    added some templating

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,6 +2,16 @@ var $ = require('jquery');
 var Backbone = require('backbone');
 var handlebars = require('handlebars');
 
+//==============================================================================
+//                        Templates
+//==============================================================================
+var formTem = require('../templates/formtemplate.handlebars');
+var contactsTem = require('../templates/contactlist.handlebars');
+
+//==============================================================================
+//                        Models
+//==============================================================================
+
 var ContactModel = Backbone.Model.extend({
 
 });
@@ -11,24 +21,35 @@ var ContactCollection = Backbone.Collection.extend({
    }
 );
 
+//==============================================================================
+//                        Views
+//==============================================================================
+
 var ContactListItemView = Backbone.View.extend({
-
   tagName: "li",
-
   className: "contact-list-items",
-
   events: {
     // "click .contact-list-items": "open",
     // "click .contact-list-items.edit": "openEditDialog",
     // "click .contact-list-items.delete": "destroy"
   },
-
   initialize: function() {
-
+    $('.contact-form').html(formTem({}));
   },
-
   render: function() {
 
   }
+});
 
+var contactFormView = Backbone.View.extend({
+  tagName: "form",
+  className: "contact-form",
+  events: {
+  },
+  initialize: function() {
+    this.$el.html(formTem({}));
+  },
+  render: function() {
+
+  }
 });
